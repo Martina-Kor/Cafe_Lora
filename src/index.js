31e34046cc215f50156a42f6eea89b0e836eaa22
Martina Korandova <martina.maserkap8@gmail.com>
2021-04-24 14:45:10 +0200

    všechny úkoly do šestého

--- src/index.js
+++ src/index.js
@@ -1,3 +1,47 @@
 import './style.css';
+//import './Layer/index';
 
-console.log('funguju!');
+//console.log('funguju');
+
+// 3) Zprovoznění navigace
+
+const navBtnElm = document.querySelector('#nav-btn');
+//console.log(navBtnElm);
+
+const navElm = document.querySelector('#nav');
+
+navBtnElm.addEventListener('click', () => {
+  navElm.classList.toggle('nav-closed');
+});
+
+const navLinkElms = document.querySelectorAll('.nav-link');
+for (let i = 0; i < navLinkElms.length; i++) {
+  navLinkElms[i].addEventListener('click', () => {
+    navElm.classList.add('nav-closed');
+  });
+}
+
+// 4) Objednávání
+const orderBtnElm = document.querySelector('#order-btn');
+//console.log(orderBtnElm);
+
+const drinkCupElm = document.querySelector('.drink__cup');
+
+//console.log(drinkCupElm);
+
+let ordered = false;
+//console.log(ordered);
+
+orderBtnElm.addEventListener('click', () => {
+  if (!ordered) {
+    drinkCupElm.classList.add('drink__cup--selected');
+    orderBtnElm.textContent = 'Zrušit';
+    ordered = true;
+  } else {
+    drinkCupElm.classList.remove('drink__cup--selected');
+    orderBtnElm.textContent = 'Objednat';
+    ordered = false;
+  }
+});
+
+// Ingredience jako komponenty
